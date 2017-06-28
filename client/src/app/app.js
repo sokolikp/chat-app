c3a335c897c2764eb0e103ebc8146b661effa574
Paul Sokolik <paul.sokolik@gmail.com>
2017-06-28 10:53:35 -0700

    add 404 route

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
 // components
 import Home from '../components/home/home';
@@ -11,17 +11,26 @@ import './app.css'
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="app">
-          <nav className="app-nav bg-info navbar navbar-light">
-            <a href="/" className="nav-home">Chat App</a>
-          </nav>
-          <Route exact path="/" component={Home}/>
-          <Route path="/chat/:chatRoomId" component={ChatRoom}/>
-        </div>
-      </Router>
+      <div className="app">
+        <nav className="app-nav bg-info navbar navbar-light">
+          <a href="/" className="nav-home">Chat App</a>
+        </nav>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/chat/:chatRoomId" component={ChatRoom}/>
+            <Route path="*" component={fourOhFour}/>
+          </Switch>
+        </Router>
+      </div>
     );
   }
+}
+
+const fourOhFour = () => {
+  return (
+    <div className="fourOhFour">Uh oh! It looks like that page doesn't exist. Go <Link to="/">home</Link>.</div>
+  );
 }
 
 export default App;

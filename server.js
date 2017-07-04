3b23d5c2d188e089cf425aeedc357ea458c6db19
Paul Sokolik <paul.sokolik@gmail.com>
2017-07-04 15:08:31 -0700

    serve index.html files on all GET routes

--- server.js
+++ server.js
@@ -1,29 +1,48 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
 
 app.set("port", process.env.PORT || 3001);
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // make socketio available to all our routes
 app.set('socketio', io);
 
 // serve static assets in production - otherwise React server handles asset serving
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(__dirname + "/client/build"));
 }
-
-http.listen(app.get("port"), () => {
-  console.log(`Find the server at: http://localhost:${app.get("port")}/`);
-});
 
 /////////////
 //// routes
 /////////////
-require('./server/routes/routes.js')(app);
+app.use(require('./server/index.js'));
+app.get('/*', function (req, res) {
+  console.log("RENDING CHAT 1!", __dirname + '/client/build/index.html');
+  res.sendFile(__dirname + '/client/build/index.html');
+});
+
+// app.get('/chat/:id', function (req, res) {
+//   console.log("RENDING CHAT 2!");
+//   res.render(__dirname + '/client/build/');
+// });
+
+// serve index file on all routes
+app.get('/*', function (req, res) {
+  res.sendFile(__dirname + '/client/build/index.html');
+});
 
 /////////////
 //// sockets
 /////////////
 require('./server/socket.js')(io);
 
+server.listen(app.get("port"), () => {
+  console.log(`Find the server at: http://localhost:${app.get("port")}/`);
+});
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,12 @@
-const bodyParser = require('body-parser');
+const router = require('express').Router();
 const userHandler = require('./user-handler.js');
 const roomHandler = require('./room-handler.js');
 const messageHandler = require('./message-handler.js');
 
-module.exports = (app) => {
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+// structure API in the form: /<handler>/<handler route>
+// e.g. /user/users for the users route within the user handler
+router.use('/user', userHandler);
+router.use('/room', roomHandler);
+router.use('/message', messageHandler);
 
-  // structure API in the form: /<handler>/<handler route>
-  // e.g. /user/users for the users route within the user handler
-  app.use('/api/user', userHandler);
-  app.use('/api/room', roomHandler);
-  app.use('/api/message', messageHandler);
-};
+module.exports = router;
